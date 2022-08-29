58fa7c01690923c4e6d400742681bcb31de33dae
4c3y <69460051+4c3y@users.noreply.github.com>
2022-08-29 17:21:09 +0200

    Added all working log combinations in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,23 @@ int main(int argc, char **argv) {
 
   int a = 3;
   int b = 2;
+
   LOG(I, "Hello log++. Found: " << a << " cookies");
+  LOG(W, "Hello log++. Found:" << a << " cookies");
+  LOG(E, "Hello log++. Found:" << a << " cookies");
+  //LOG(F, "Hello log++. Found:" << a << " cookies");
+
   LOG(INFO) << "Hello glog. Found: " << b << " cookies";
   LOG(WARNING) << "Hello glog. Found: " << b << " cookies";
   LOG(ERROR) << "Hello glog. Found: " << b << " cookies";
+  //LOG(FATAL) << "Hello glog. Found: " << b << " cookies";
 
   ROS_INFO("Hello Ros");
-
-  LOG(FATAL) << "Hello glog. Found: " << b << " cookies";
+  ROS_INFO_STREAM("Hello Ros");
+  ROS_WARN("Hello Ros");
+  ROS_WARN_STREAM("Hello Ros");
+  ROS_ERROR("Hello Ros");
+  ROS_ERROR_STREAM("Hello Ros");
+//  ROS_FATAL("Hello Ros");
+//  ROS_FATAL_STREAM("Hello Ros");
 }
