68b0acb5510de193619e19452c4cf8b24eacf474
4c3y <69460051+4c3y@users.noreply.github.com>
2022-09-05 14:32:01 +0200

    Added unittests in test_basic_roslog.cc

--- test/roslog/test_basic_roslog.cc
+++ test/roslog/test_basic_roslog.cc
@@ -9,13 +9,63 @@
 #define MODE_ROSLOG
 #include "log++.h"
 
-//TODO
-TEST(roslog_glog_syntax, severity_info) {
-  LOG_INIT(*test_argv);
+/**
+ * Ros logging stdout/stderr capture doesn't work. Probably because of threading things.
+ * Unittests only cover ros function calls through Log++ at the moment.
+ */
 
-  testing::internal::CaptureStderr();
-  LOG(INFO) << "xyz";
-  std::string output = testing::internal::GetCapturedStderr();
-  LOG(INFO) << output;
+//! ################ GLOG ################
+TEST(roslog_glog_syntax, severity_info) {
+  LOG(INFO) << "Test";
+}
+
+TEST(roslog_glog_syntax, severity_warning) {
+  LOG(WARNING) << "Test";
+}
+
+TEST(roslog_glog_syntax, severity_error) {
+  LOG(ERROR) << "Test";
+}
+
+TEST(roslog_glog_syntax, severity_fatal) {
+  LOG(FATAL) << "Test";
+}
+
+//! ################ lpp ################
+TEST(roslog_lpp_syntax, severity_info) {
+  LOG(I, "" << "Test");
+}
+
+TEST(roslog_lpp_syntax, severity_warning) {
+  LOG(W, "" << "Test");
+}
+
+TEST(roslog_lpp_syntax, severity_error) {
+  LOG(E, "" << "Test");
+}
+
+TEST(roslog_lpp_syntax, severity_fatal) {
+  LOG(F, "" << "Test");
+}
+
+//! ################ Roslog ################
+TEST(roslog_roslog_syntax, severity_info) {
+  ROS_INFO("Test");
+  ROS_INFO_STREAM("" << "Test");
+}
+
+TEST(roslog_roslog_syntax, severity_warn) {
+  ROS_WARN("Test");
+  ROS_WARN_STREAM("" << "Test");
+}
+
+TEST(roslog_roslog_syntax, severity_error) {
+  ROS_ERROR("Test");
+  ROS_ERROR_STREAM("" << "Test");
+}
+
+TEST(roslog_roslog_syntax, severity_fatal) {
+  ROS_FATAL("Test");
+  ROS_FATAL_STREAM("" << "Test");
 }
 #undef MODE_ROSLOG
