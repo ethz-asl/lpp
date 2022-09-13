3da601a299495cc665f873f95cf9151c801741bf
4c3y <69460051+4c3y@users.noreply.github.com>
2022-09-13 15:52:59 +0200

    Fixed LPP_CAPTURE macros

--- test/common/test_utils.h
+++ test/common/test_utils.h
@@ -15,10 +15,10 @@ inline static bool isSubstring(const std::string& string, const std::string& sub
 }
 
 //! Macros to capture stdout and stderr and assign output directly to std::string
-#define LPP_CAPTURE_STDOUT(x) []() {testing::internal::CaptureStdout(); (x); \
+#define LPP_CAPTURE_STDOUT(x) []() {testing::internal::CaptureStdout(); x; \
 return testing::internal::GetCapturedStdout();}()
 
-#define LPP_CAPTURE_STDERR_LOG(x) []() {testing::internal::CaptureStderr(); (x); \
+#define LPP_CAPTURE_STDERR(x) []() {testing::internal::CaptureStderr(); x; \
 return testing::internal::GetCapturedStderr();}()
 
 #endif //LOG_TEST_TEST_UTILS_H_
