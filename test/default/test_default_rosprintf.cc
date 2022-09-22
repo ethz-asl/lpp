ead30aa71c9a037c9bf9130ca9d427c2726320b5
4c3y <69460051+4c3y@users.noreply.github.com>
2022-09-22 18:04:52 +0200

    Added output assertion for all tests, added tests for ROS_THROTTLE, added tests for fatal severity in test_default_rosprintf.cc

--- test/default/test_default_rosprintf.cc
+++ test/default/test_default_rosprintf.cc
@@ -5,35 +5,117 @@
 #include <gtest/gtest.h>
 #include <test_utils.h>
 #include <log++.h>
+inline static constexpr const char *ERROR_MESSAGE = "Base angle (%f) is less than the minimum angle (%f)";
+
+namespace eq {
+inline static const std::string debug = "\x1B[m[DEBUG] [.]: Base angle (.) is less than the minimum angle (.)\x1B[m\n";
+inline static const std::string info = "\x1B[m[ INFO] [.]: Base angle (.) is less than the minimum angle (.)\x1B[m\n";
+inline static const std::string
+    warning = "\x1B[m[ WARN] [.]: Base angle (.) is less than the minimum angle (.)\x1B[m\n";
+inline static const std::string error = "\x1B[m[ERROR] [.]: Base angle (.) is less than the minimum angle (.)\x1B[m\n";
+inline static const std::string fatal = "\x1B[m[FATAL] [.]: Base angle (.) is less than the minimum angle (.)\x1B[m\n";
+}
 
 TEST(default_rosprintf, ros_debug) {
-  ROS_DEBUG("Base angle (%f) is less than the minimum angle (%f)", 3.3, 5.5);
+  LOG_INIT(*test_argv);
+
+  std::string output = LPP_CAPTURE_STDOUT(ROS_DEBUG(ERROR_MESSAGE, 3.3, 5.5));
+  ASSERT_EQ(eq::debug, removeNumbersFromString(output));
 }
 
 TEST(default_rosprintf, ros_debug_once) {
-  ROS_DEBUG_ONCE("Base angle (%f) is less than the minimum angle (%f)", 3.3, 5.5);
+  LOG_INIT(*test_argv);
+
+  std::string output = LPP_CAPTURE_STDOUT(ROS_DEBUG_ONCE(ERROR_MESSAGE, 3.3, 5.5));
+  ASSERT_EQ(eq::debug, removeNumbersFromString(output));
+}
+
+TEST(default_rosprintf, ros_debug_throttle) {
+  LOG_INIT(*test_argv);
+
+  std::string output = LPP_CAPTURE_STDOUT(ROS_DEBUG_THROTTLE(1, ERROR_MESSAGE, 3.3, 5.5));
+  ASSERT_EQ(eq::debug, removeNumbersFromString(output));
 }
 
 TEST(default_rosprintf, ros_info) {
-  ROS_INFO("Base angle (%f) is less than the minimum angle (%f)", 3.3, 5.5);
+  LOG_INIT(*test_argv);
+
+  std::string output = LPP_CAPTURE_STDOUT(ROS_INFO(ERROR_MESSAGE, 3.3, 5.5));
+  ASSERT_EQ(eq::info, removeNumbersFromString(output));
 }
 
 TEST(default_rosprintf, ros_info_once) {
-  ROS_INFO_ONCE("Base angle (%f) is less than the minimum angle (%f)", 3.3, 5.5);
+  LOG_INIT(*test_argv);
+
+  std::string output = LPP_CAPTURE_STDOUT(ROS_INFO_ONCE(ERROR_MESSAGE, 3.3, 5.5));
+  ASSERT_EQ(eq::info, removeNumbersFromString(output));
+}
+
+TEST(default_rosprintf, ros_info_throttle) {
+  LOG_INIT(*test_argv);
+
+  std::string output = LPP_CAPTURE_STDOUT(ROS_INFO_THROTTLE(1, ERROR_MESSAGE, 3.3, 5.5));
+  ASSERT_EQ(eq::info, removeNumbersFromString(output));
 }
 
 TEST(default_rosprintf, ros_warn) {
-  ROS_WARN("Base angle (%f) is less than the minimum angle (%f)", 3.3, 5.5);
+  LOG_INIT(*test_argv);
+
+  std::string output = LPP_CAPTURE_STDERR(ROS_WARN(ERROR_MESSAGE, 3.3, 5.5));
+  ASSERT_EQ(eq::warning, removeNumbersFromString(output));
 }
 
 TEST(default_rosprintf, ros_warn_once) {
-  ROS_WARN_ONCE("Base angle (%f) is less than the minimum angle (%f)", 3.3, 5.5);
+  LOG_INIT(*test_argv);
+
+  std::string output = LPP_CAPTURE_STDERR(ROS_WARN_ONCE(ERROR_MESSAGE, 3.3, 5.5));
+  ASSERT_EQ(eq::warning, removeNumbersFromString(output));
+}
+
+TEST(default_rosprintf, ros_warn_throttle) {
+  LOG_INIT(*test_argv);
+  std::string output = LPP_CAPTURE_STDERR(ROS_WARN_THROTTLE(1, ERROR_MESSAGE, 3.3, 5.5));
+  ASSERT_EQ(eq::warning, removeNumbersFromString(output));
 }
 
 TEST(default_rosprintf, ros_error) {
-  ROS_ERROR("Base angle (%f) is less than the minimum angle (%f)", 3.3, 5.5);
+  LOG_INIT(*test_argv);
+
+  std::string output = LPP_CAPTURE_STDERR(ROS_ERROR(ERROR_MESSAGE, 3.3, 5.5));
+  ASSERT_EQ(eq::error, removeNumbersFromString(output));
 }
 
 TEST(default_rosprintf, ros_error_once) {
-  ROS_ERROR_ONCE("Base angle (%f) is less than the minimum angle (%f)", 3.3, 5.5);
+  LOG_INIT(*test_argv);
+
+  std::string output = LPP_CAPTURE_STDERR(ROS_ERROR_ONCE(ERROR_MESSAGE, 3.3, 5.5));
+  ASSERT_EQ(eq::error, removeNumbersFromString(output));
+}
+
+TEST(default_rosprintf, ros_error_throttle) {
+  LOG_INIT(*test_argv);
+
+  std::string output = LPP_CAPTURE_STDERR(ROS_ERROR_THROTTLE(1, ERROR_MESSAGE, 3.3, 5.5));
+  ASSERT_EQ(eq::error, removeNumbersFromString(output));
+}
+
+TEST(default_rosprintf, ros_fatal) {
+  LOG_INIT(*test_argv);
+
+  std::string output = LPP_CAPTURE_STDERR(ROS_FATAL(ERROR_MESSAGE, 3.3, 5.5));
+  ASSERT_EQ(eq::fatal, removeNumbersFromString(output));
+}
+
+TEST(default_rosprintf, ros_fatal_once) {
+  LOG_INIT(*test_argv);
+
+  std::string output = LPP_CAPTURE_STDERR(ROS_FATAL_ONCE(ERROR_MESSAGE, 3.3, 5.5));
+  ASSERT_EQ(eq::fatal, removeNumbersFromString(output));
+}
+
+TEST(default_rosprintf, ros_fatal_throttle) {
+  LOG_INIT(*test_argv);
+
+  std::string output = LPP_CAPTURE_STDERR(ROS_FATAL_THROTTLE(1, ERROR_MESSAGE, 3.3, 5.5));
+  ASSERT_EQ(eq::fatal, removeNumbersFromString(output));
 }
