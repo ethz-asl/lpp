9d2df5e13ed24e34e7ff53d302c4330a5fae62e9
4c3y <69460051+4c3y@users.noreply.github.com>
2022-09-22 17:29:50 +0200

    Added glog tests with severity debug/fatal, fixed generateTests()

--- test/default/test_default_every_n.cc
+++ test/default/test_default_every_n.cc
@@ -121,3 +121,20 @@ TEST(default_LogEveryN, glog_syntax_severity_error) {
     }
   }
 }
+
+TEST(default_LogEveryN, glog_syntax_severity_fatal) {
+  LOG_INIT(*test_argv);
+
+  std::function<void()> fn = [](){
+    for (int i = 0; i < 5; i++) {
+      std::string output = LPP_CAPTURE_STDERR(LOG_EVERY_N(FATAL, 3) << "Test" << 123);
+
+      if (i % 3 == 0) {
+        ASSERT_TRUE(isSubstring(output, "Test123"));
+      } else {
+        ASSERT_EQ(output, "");
+      }
+    }
+  };
+  ASSERT_TRUE(checkAbort(fn));
+}
