73946158fdb60fbad093c6ec19b71b3536232f59
4c3y <69460051+4c3y@users.noreply.github.com>
2022-09-29 13:02:33 +0200

    Renamed main.cpp to demo.cpp and added example

---
+++ src/demo.cpp
@@ -0,0 +1,16 @@
+//
+// Created by 4c3y (acey) on 18.08.22.
+//
+
+#include <log++.h>
+
+int main(int argc, char **argv) {
+  LOG_INIT(argv[0]);
+
+  int foo = 5;
+
+  LOG(I, "Foo: " << foo);           //Log++ syntax
+  ROS_INFO_STREAM("Foo: " << foo);  //ROS syntax
+  LOG(INFO) << "Foo: " << foo;      //Glog syntax
+  return 0;
+}
