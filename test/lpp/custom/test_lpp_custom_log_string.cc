916ee2f48da47e7915ae5f67ade53b33b4aac9b3
4c3y <69460051+4c3y@users.noreply.github.com>
2022-11-24 11:23:21 +0100

    Added test_lpp_custom_log_string.cc

---
+++ test/lpp/custom/test_lpp_custom_log_string.cc
@@ -0,0 +1,94 @@
+//
+// Created by 4c3y (acey) on 24.11.22.
+//
+
+#include <gtest/gtest.h>
+#include <log++.h>
+#include <test_utils.h>
+#include "callback.h"
+
+using namespace lpp::custom;
+
+TEST(lpp_log_string, severity_info) {
+  LOG_INIT(*test_argv, logCallback);
+
+  std::vector<std::string> errors;
+
+//Can't capture variables in lambda with LPP_CAPTURE_STDERR()
+  testing::internal::CaptureStdout();
+  LOG_STRING(INFO, &errors) << "test" << 123;
+  std::string output = testing::internal::GetCapturedStdout();
+
+  ASSERT_EQ(output, "");
+  ASSERT_EQ("test123", errors.at(0));
+}
+
+TEST(lpp_log_string, severity_info_null) {
+  LOG_INIT(*test_argv, logCallback);
+
+  std::string output = LPP_CAPTURE_STDOUT(LOG_STRING(INFO, nullptr) << "test" << 123);
+  ASSERT_EQ(output, info);
+}
+
+TEST(lpp_log_string, severity_warning) {
+  LOG_INIT(*test_argv, logCallback);
+
+  std::vector<std::string> errors;
+
+//Can't capture variables in lambda with LPP_CAPTURE_STDERR()
+  testing::internal::CaptureStdout();
+  LOG_STRING(WARNING, &errors) << "test" << 123;
+  std::string output = testing::internal::GetCapturedStdout();
+
+  ASSERT_EQ(output, "");
+  ASSERT_EQ("test123", errors.at(0));
+}
+
+TEST(lpp_log_string, severity_warning_null) {
+  LOG_INIT(*test_argv, logCallback);
+
+  std::string output = LPP_CAPTURE_STDOUT(LOG_STRING(WARNING, nullptr) << "test" << 123);
+  ASSERT_EQ(output, warning);
+}
+
+TEST(lpp_log_string, severity_error) {
+  LOG_INIT(*test_argv, logCallback);
+
+  std::vector<std::string> errors;
+
+//Can't capture variables in lambda with LPP_CAPTURE_STDERR()
+  testing::internal::CaptureStdout();
+  LOG_STRING(ERROR, &errors) << "test" << 123;
+  std::string output = testing::internal::GetCapturedStdout();
+
+  ASSERT_EQ(output, "");
+  ASSERT_EQ("test123", errors.at(0));
+}
+
+TEST(lpp_log_string, severity_error_null) {
+  LOG_INIT(*test_argv, logCallback);
+
+  std::string output = LPP_CAPTURE_STDOUT(LOG_STRING(ERROR, nullptr) << "test" << 123);
+  ASSERT_EQ(output, error);
+}
+
+TEST(lpp_log_string, severity_fatal) {
+  LOG_INIT(*test_argv, logCallback);
+
+  std::vector<std::string> errors;
+
+//Can't capture variables in lambda with LPP_CAPTURE_STDERR()
+  testing::internal::CaptureStdout();
+  LOG_STRING(FATAL, &errors) << "test" << 123;
+  std::string output = testing::internal::GetCapturedStdout();
+  ASSERT_EQ(output, "");
+
+  ASSERT_EQ("test123", errors.at(0));
+}
+
+TEST(lpp_log_string, severity_fatal_null) {
+  LOG_INIT(*test_argv, logCallback);
+
+  std::string output = LPP_CAPTURE_STDOUT(LOG_STRING(FATAL, nullptr) << "test" << 123);
+  ASSERT_EQ(output, fatal);
+}
