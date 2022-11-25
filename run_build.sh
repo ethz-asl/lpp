3705eff4ac40d83749240192f03aef5036058c81
4c3y <69460051+4c3y@users.noreply.github.com>
2022-11-25 11:12:42 +0100

    Added ros noetic installation

--- run_build.sh
+++ run_build.sh
@@ -1,3 +1,30 @@
+#!/bin/bash -e
+
+CATKIN_PATH="$(which catkin)"
+
+if [ "$CATKIN_PATH" = "" ]; then
+    echo "Installing ros noetic"
+
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+    sudo apt install curl
+    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+    sudo apt update
+    sudo apt install ros-noetic-ros-base
+
+    source /opt/ros/noetic/setup.bash
+
+    sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+    sudo apt install python3-catkin-tools
+
+    sudo rosdep init
+    rosdep update
+
+else
+  echo "catkin found"
+fi
+
+exit
+
 export DEBIAN_FRONTEND=noninteractive
 sudo apt install -y libgoogle-glog-dev 
 BUILD_DIR=cmake-build
