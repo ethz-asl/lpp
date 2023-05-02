8db36f471487d0309657712d06fe1706ab004dc0
4c3y <69460051+4c3y@users.noreply.github.com>
2023-05-02 11:22:14 +0200

    Build ros from source in ubuntu 22.04 workflow

--- .github/scripts/run_build_ubuntu22_04.sh
+++ .github/scripts/run_build_ubuntu22_04.sh
@@ -3,18 +3,12 @@ CATKIN_PATH="$(which catkin)"
 if [ "$CATKIN_PATH" = "" ]; then
     echo "Installing ros noetic"
 
-    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-    sudo apt install curl
-    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-    sudo apt update
-    sudo apt install ros-noetic-ros-base
-
-    source /opt/ros/noetic/setup.bash
-
-    sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential python3-catkin-tools libgoogle-glog-dev
-
-    sudo rosdep init
-    rosdep update
+    git clone git@github.com:lucasw/ros_from_src.git
+    mkdir build
+    cd build
+    ROSCONSOLE=https://github.com/ros/rosconsole ../ros_from_src/git_clone.sh
+    sudo ../ros_from_src/dependencies.sh
+    ../ros_from_src/build.sh
 
 else
   sudo apt install -y libgoogle-glog-dev
