169bd7ea719f8af77c3fe64f1e237e88cc64ee6c
4c3y <69460051+4c3y@users.noreply.github.com>
2024-04-05 13:40:08 +0200

    Added unittests for timed logging with floating point periods

--- test/glog/test_glog_timed.cc
+++ test/glog/test_glog_timed.cc
@@ -34,6 +34,19 @@ std::vector<AsyncTest> generateTests() {
   };
 }
 
+TEST(glog_timed, lpp_syntax_floating_point_time) {
+  for (int i = 0; i < 5; i++) {
+
+    std::string output = LPP_CAPTURE_STDOUT(LOG_TIMED(I, 0.1, "Test" << 123));
+    if (i % 2 == 0) {
+      ASSERT_TRUE(isSubstring(output, "I"));
+      ASSERT_TRUE(isSubstring(output, "Test123"));
+    }
+    //sleep 0.1s
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+  }
+}
+
 TEST(glog_timed, lpp_syntax_severity_debug) {
   ASSERT_TRUE(TestResult::getInstance().get(GET_CLASS_NAME(*this, nullptr)));
 }
